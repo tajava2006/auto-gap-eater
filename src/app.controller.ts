0140c7c090fc59a123c7bb5fbd984789e6559b9e
hoppe <tajava2006@gmail.com>
2024-04-02 11:09:21 +0900

    chg : verify webhooks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,50 @@
-import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { exec } from 'child_process';
+import { ConfigService } from '@nestjs/config';
+import * as crypto from 'crypto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+  verify_signature = (req: Request) => {
+    const WEBHOOK_SECRET = this.configService.get('WEBHOOK_SECRET');
+    const signature = crypto
+      .createHmac('sha256', WEBHOOK_SECRET)
+      .update(JSON.stringify(req.body))
+      .digest('hex');
+    const trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+    const untrusted = Buffer.from(
+      req.headers.get('x-hub-signature-256'),
+      'ascii',
+    );
+    return crypto.timingSafeEqual(trusted, untrusted);
+  };
 
   @Post('/github-webhooks')
-  async githubWebhooks(@Body() body, @Res() res) {
+  async githubWebhooks(@Body() body, @Res() res, @Req() req) {
     console.log('body :', body);
     // Git에서 코드 풀하기
+    if (!this.verify_signature(req)) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
     exec('git pull', (err) => {
       if (err) {
         console.error(`Git pull error: ${err}`);
